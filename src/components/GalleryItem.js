4bf684f2bf8369634bf7adad09e55d455e14e541
Ashley Vieira <ash.adr24212@gmail.com>
2022-09-19 19:34:23 -0400

    styling added & search as you type

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,9 +3,27 @@ import { useState } from 'react'
 function GalleryItem(props) {
     let [view, setView] = useState(false)
 
+    const simpleStyle = {
+        'width': '25vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px'
+    }
+
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
+
     const simpleView = () => {
         return (
-            <div>
+            <div style={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -25,11 +43,10 @@ function GalleryItem(props) {
 
     return (
         <div onClick={() => setView(!view)}
-        style={{'display': 'inline-block'}}>
+            style={{'display': 'inline-block'}}>
         
             {/* This simple ternary shows the simple view when 'view' is false! */}
             {view ? detailView() : simpleView()}
-
         </div>
     )
 }
